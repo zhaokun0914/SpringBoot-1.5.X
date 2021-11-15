44aab89d89c5e9f261da03f013d8c7fcd7b76346
zhaokun <283798272@qq.com>
2021-11-15 14:28:51 +0800

    完成跳转到添加页面

--- spring-boot-04-restfulcrud/src/main/java/com/fortunebill/springboot/controller/EmployeeController.java
+++ spring-boot-04-restfulcrud/src/main/java/com/fortunebill/springboot/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.fortunebill.springboot.controller;
 
+import com.fortunebill.springboot.dao.DepartmentDao;
 import com.fortunebill.springboot.dao.EmployeeDao;
+import com.fortunebill.springboot.entities.Department;
 import com.fortunebill.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,13 +21,23 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Map<String, Object> map) {
         Collection<Employee> all = employeeDao.getAll();
-
         map.put("emps", all);
         // thymeleaf默认就会拼串，从classpath:/templates/emp/list.html
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Map<String, Object> map) {
+        // 来到添加页面之前，先查出所有部门
+        Collection<Department> departments = departmentDao.getDepartments();
+        map.put("depts", departments);
+        return "emp/add";
     }
 
 }
