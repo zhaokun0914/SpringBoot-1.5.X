30da768238cf4947284a0b512e7489ebd6f44b51
zhaokun <283798272@qq.com>
2021-11-16 20:05:46 +0800

    使用外置的Servlet容器

--- spring-boot-04-restfulcrud/src/main/java/com/fortunebill/springboot/SpringBoot04RestfulcrudApplication.java
+++ spring-boot-04-restfulcrud/src/main/java/com/fortunebill/springboot/SpringBoot04RestfulcrudApplication.java
@@ -1,11 +1,25 @@
 package com.fortunebill.springboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.Banner;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.ParentContextApplicationContextInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
+import org.springframework.boot.diagnostics.FailureAnalyzers;
+import org.springframework.boot.web.support.ServletContextApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.util.Assert;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.StandardServletEnvironment;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Locale;
 
