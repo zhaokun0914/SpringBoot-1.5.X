12f2267a227833eade39fa00c9ecf75086a53156
zhaokun <283798272@qq.com>
2021-11-18 16:30:03 +0800

    测试自定义starter

--- spring-boot-08-starter-test/src/test/java/com/fortunebill/springboot/SpringBoot08StarterTestApplicationTests.java
+++ spring-boot-08-starter-test/src/test/java/com/fortunebill/springboot/SpringBoot08StarterTestApplicationTests.java
@@ -1,13 +1,8 @@
 package com.fortunebill.springboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBoot08StarterTestApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
